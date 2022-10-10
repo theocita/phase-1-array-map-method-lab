9b3f9ff9ba8e31c6f9e820b5516841aefcd0e503
theocita <theocitaallen@gmail.com>
2022-10-10 19:27:19 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str => str.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
 }
